e3980d407d2f60840c79209f0cc44507552b4115
Titc <1240104681@qq.com>
2023-04-07 11:11:51 +0800

    home 开发

--- main.js
+++ main.js
@@ -3,6 +3,36 @@
 import Vue from 'vue'
 import App from './App'
 
+// 导入2网络请求包
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http=$http
+
+$http.baseUrl = 'https://api-hmugo-web.itheima.net'
+
+uni.$showMsg=function(title = '数据加载失败！', duration = 1500){
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none'
+  })
+}
+
+
+// 请求拦截器
+$http.beforeRequest=function(options){
+  uni.showLoading({
+    title:'数据加载中'
+  })
+}
+
+//响应拦截器
+$http.afterRequest = function () {
+uni.hideLoading()
+}
+
+
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
